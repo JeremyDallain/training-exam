fe8694b947d9d1cff00ae26891d71abe70fe91bd
JeremyDallain <jeremy.dallain@gmail.com>
2021-03-20 11:15:52 +0100

    mise en place de la base, auth et user

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class SecurityController extends AbstractController
+{
+    /**
+     * @Route("/login", name="app_login")
+     */
+    public function login(AuthenticationUtils $authenticationUtils): Response
+    {
+        if ($this->getUser()) {
+            $this->addFlash('danger', 'Vous êtes déjà connecté en tant que : '.$this->getUser()->getEmail());
+            return $this->redirectToRoute('home');
+        }
+
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+    }
+
+    /**
+     * @Route("/logout", name="app_logout")
+     */
+    public function logout()
+    {
+        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+    }
+
+    /**
+     * @Route("/logout/action", name="app_logout_action")
+     */
+    public function logoutAction()
+    {
+        $this->addFlash('secondary', 'Vous êtes déconnecté !');
+        return $this->redirectToRoute('home');
+    }
+}
